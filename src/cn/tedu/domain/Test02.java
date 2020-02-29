b80fedabdcbbb278a40c31911e16266fd25bb118
Niklaus <1525104690@qq.com>
2020-02-29 17:15:04 +0800

    增加test02.java

---
+++ src/cn/tedu/domain/Test02.java
@@ -0,0 +1,12 @@
+package cn.tedu.domain;
+
+/**
+ * @author XiangRui
+ * @create 2020-02-29 5:14 下午
+ */
+public class Test02 {
+    public static void main(String[] args) {
+        System.out.println("xxx");
+        System.out.println("yyy");
+    }
+}
