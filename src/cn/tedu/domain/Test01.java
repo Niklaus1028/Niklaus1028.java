91dbc3ab5d4c1b74df3f18a9d837f897904a9ccc
Niklaus <1525104690@qq.com>
2020-02-29 16:16:29 +0800

    Test01.java

---
+++ src/cn/tedu/domain/Test01.java
@@ -0,0 +1,13 @@
+package cn.tedu.domain;
+
+/**
+ * @author XiangRui
+ * @create 2020-02-29 4:14 下午
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+        System.out.println("bbb");
+        System.out.println("ccc");
+    }
+}
